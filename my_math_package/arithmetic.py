856a0c663ebf74b840325d6f09f91017b194cfe2
Baddynicki <140972073+Baddynicki@users.noreply.github.com>
2024-04-25 21:54:09 +0530

    Add files via upload

---
+++ my_math_package/arithmetic.py
@@ -0,0 +1,12 @@
+#add(a, b): Adds two numbers and returns the result
+def add(a, b):
+    return(a + b)
+#subtract(a, b): Subtracts the second number from the first and returns the result.
+def subtract(a, b):
+    return(a - b)
+#multiply(a, b): Multiplies two numbers and returns the result.
+def multiply(a,b):
+    return(a*b)
+#divide(a, b): Divides the first number by the second and returns the result.
+def divide(a,b):
+    return(a/b)
