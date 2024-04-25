868dd4aa0a89aaa31af4c3b94e5b5eefaeb788e5
Baddynicki <140972073+Baddynicki@users.noreply.github.com>
2024-04-25 21:55:23 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages # type: ignore
-setup(
-    name='my_math_package',
-    version='0.1',
-    author="Bhavya",
-    description="This is a module containing basic mathematical functions.",
-    packages=find_packages(),
-)
